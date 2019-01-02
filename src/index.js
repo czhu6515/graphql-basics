09bf726cc990f153b06d6210c2ded2c7005424e6
Chen <chenyu.zhu86@gmail.com>
2019-01-02 18:53:36 -0500

    created query definitions and resolvers in index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,43 @@
 import { GraphQLServer } from 'graphql-yoga'
 
-//type definitions (schema)
+// Scalar Types = String, Boolean, Int, Float, ID
 
+// Type definitions (schema)
+const typeDefs = `
+  type Query {
+    title: String!
+    price: Float!
+    releaseYear: Int
+    rating: Float
+    inStock: Boolean!
+  }
+`
 
+// Resolvers
+const resolvers = {
+  Query: {
+    title(){
+      return 'A Feast for Crows'
+    },
+    price(){
+      return 23.99
+    },
+    releaseYear(){
+      return 2010
+    },
+    rating(){
+      return null
+    },
+    inStock(){
+      return false
+    }
+  }
+}
 
-//resolvers
+const server = new GraphQLServer({
+  typeDefs, resolvers
+})
+
+server.start(() => {
+  console.log('server is functional')
+})
